f289299f972ffe4d778e17f813581ea4d39be06f
DhanushSS <67519006+DhanushSS@users.noreply.github.com>
2020-12-17 19:00:29 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,47 @@
+![Image description](https://i1.faceprep.in/ProGrad/face-logo-resized.png)
+
+# ProGrad Lab | BRRRGRRR
+
+Mama Jenny is the best cook you would ever met. She makes the most amazing burgers in her tiny diner across the street. Mama is extremely popular and has recently had the idea to take her business online. Unfortunately, she isn't the best at web development. Mama has heard of ProGrads and she thought you could do a great job. 
+
+Help Mama create a website that'll attract more audience and help her make a living through it. Go to the `src/app.js` file and complete all the unfinished code to let her live here dream.
+
+## What should you do
+```
+Fork this repo
+Clone this repo
+Practice DOM Features
+```
+
+## How To Submit
+```
+Upon completion, run the following commands:
+
+git add .
+git commit -m "ProGrad ID"
+git push origin master
+
+And finally, create a pull request so your ProGrad Mentor (PM) can review your work.
+```
+
+### Progression 1: LAYERS
+
+As your first task, you've got to enable the different layers of the burger on the screen. Create a `render()` for each ingredient and make sure that it displays the selected layer.
+
+### Progression 2: LAYER THEM UP
+
+Setup `EventListeners`, so that we can add or remove different ingredients by clicking on the respective ingredient buttons. That's the whole point of it.
+
+### Challenge 1: IT GETS CHEESEY
+
+Make the ingredient buttons pop up based on their current active state. This would help customers identify what ingredients they've selected and what's left out.
+
+### Challenge 2: TAKE THE ORDER
+
+Show the customer only the ingredients that they've selected on the chalk board.
+
+### Challenge 3: BILL TIME
+
+The customer has had a good time customizing his brrrgrrr. Time for him to pay. Use ingredient prices to calculate the cost of a brrrgrrr and display it to the customer.
+
+Happy Coding ProGrad ❤️
---
+++ app.js
@@ -0,0 +1,125 @@
+// Write your code on this file. Please don't change the existing code
+// unless absolutely needed.
+
+//Initial price of the burger
+var wholeWheatBun = 10;
+
+//Ingredients of the burger along with the price
+// Clue: the name is same as the textcontent of the button. Will be useful later on :)
+var ingredients = {
+  Patty: 80,
+  Cheese: 10,
+  Tomatoes: 20,
+  Onions: 20,
+  Lettuce: 20
+};
+
+//Current state of the ingredients in the burger
+var state = {
+  Patty: true,
+  Cheese: true,
+  Tomatoes: true,
+  Onions: true,
+  Lettuce: true
+};
+
+// This function renders the entire screen everytime the state changes accordingly
+function renderAll() {
+  renderPatty();
+  renderCheese();
+  renderTomatoes();
+  renderOnions();
+  renderLettuce();
+  renderButtons();
+  renderIngredientsBoard();
+  renderPrice();
+}
+
+function renderPatty() {
+  let patty = document.querySelector("#patty");
+  //you can also use getElementById
+  if (state.Patty) {
+    patty.style.display = "inherit";
+  } else {
+    patty.style.display = "none";
+  }
+}
+
+function renderCheese() {
+  //Trial 1 - Change the visibility of cheese based on state by manipulating the DOM
+  let cheese = document.querySelector("#cheese")
+  if (state.Cheese) {
+    cheese.style.display = "inherit"
+  } else {
+    cheese.style.display = "none"
+  }
+}
+
+function renderTomatoes() {
+  //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
+  let tomato = document.querySelector("#tomato")
+  if (state.Tomatoes) {
+    tomato.style.display = "inherit"
+  } else {
+    tomato.style.display = "none"
+  }
+}
+
+function renderOnions() {
+  //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
+  let onion = document.querySelector("#onion")
+  if (state.Onions) {
+    onion.style.display = "inherit"
+  } else {
+    onion.style.display = "none"
+  }
+}
+
+function renderLettuce() {
+  //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
+  let lettuce = document.querySelector("#lettuce")
+  if (state.Lettuce) {
+    lettuce.style.display = "inherit"
+  } else {
+    lettuce.style.display = "none"
+  }
+}
+
+document.querySelector(".btn-patty").onclick = function () {
+  state.Patty = !state.Patty;
+  renderAll();
+};
+
+// Trial 2 - Setup event listener for the cheese button
+document.querySelector(".btn-cheese").onclick = function () {
+  state.Cheese = !state.Cheese;
+  renderAll();
+}
+
+// Trial 2 - Setup event listener for the tomatoes button
+document.querySelector(".btn-tomatoes").onclick = function () {
+  state.Tomatoes = !state.Tomatoes;
+  renderAll();
+}
+
+// Trial 2 - Setup event listener for the onion button
+document.querySelector(".btn-onions").onclick = function () {
+  state.Onions = !state.Onions;
+  renderAll();
+}
+
+// Trial 2 - Setup event listener for the lettuce button
+document.querySelector(".btn-lettuce").onclick = function () {
+  state.Lettuce = !state.Lettuce;
+  renderAll();
+}
+
+//Challenge 1 - Add/Remove the class active to the buttons based on state
+
+
+//Challenge 2 - Render only the items selected in the ingredients board based on the state
+
+
+//Judgement 1
+//In the p element having price-details as the class, display the calculated
+//price based on ingredients
